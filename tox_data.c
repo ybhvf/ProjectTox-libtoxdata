2dbe68b63d586eeece93a5c446413ec933c367c9
Louis Jencka <louis@jencka.us>
2014-03-12 01:44:45 -0600

    Simplified code to deal with buffers only
    This commit simplifies the library to only offer the ability to save/load
    encrypted tox messengers to buffer, fixing issue #4.

--- tox_data.c
+++ tox_data.c
@@ -1,122 +1,125 @@
 #include "tox_data.h"
 
-void _gen_key(tox_data *data, uint8_t *password, uint8_t *key) {
-    scrypt(password, strlen((char*)password), data->salt, 24, data->scrypt_n, data->scrypt_r, data->scrypt_p, key, 32);
+uint32_t tox_size_encrypted(Tox *tox) {
+    return tox_size(tox) + 108;
 }
 
-void _gen_key_new(tox_data *data, uint8_t *password) {
-    randombytes(data->salt, 24);
-    randombytes(data->nonce, 24);
-    _gen_key(data, password, data->encrypted_key);
+int tox_save_encrypted(Tox *tox, uint8_t *data, uint8_t *key, uint16_t key_length) {
+    /* The values used (N=14,r=8,p=1) were sourced from here: https://www.tarsnap.com/scrypt/scrypt-slides.pdf
+     * Percival reccommend these values for interactive logins.
+     */
+    uint32_t scrypt_n = 14,
+             scrypt_r = 8,
+             scrypt_p = 1;
+
+    uint8_t  encrypted_key[crypto_secretbox_KEYBYTES];
+             nonce[crypto_secretbox_NONCEBYTES],
+             salt[24],
+
+    uint64_t block_two_length = tox_size(tox) + 36;
+
+    //generate encryption key
+    randombytes(salt, 24);
+    randombytes(nonce, 24);
+    scrypt(key, key_length, salt, 24, scrypt_n, scrypt_r, scrypt_p, encrypted_key, 32);
+
+    uint8_t block_two_plaintext[block_two_size],
+            block_two_encrypted[block_two_size];
+
+    //prepare block two
+
+    uint8_t magic2[4] = {0x72, 0x74, 0x61, 0x73};
+    memcpy(magic2, block_two_plaintext + 32, 4);
+
+    tox_save(tox, block_two_plaintext + 32 + 4)
+
+    memset(block_two_plaintext, 0, 32);
+    if(crypto_secretbox(block_two_encrypted, block_two_plaintext, block_two_size, nonce, encrypted_key) != 0)
+        return -2;
+
+    //prepare block one
+
+    int offset = 0;
+
+    //magic
+    uint8_t magic1[4] = {0x6c, 0x69, 0x62, 0x65};
+    memcpy(magic1, data, 4, file);
+    offset += 4;
+
+    //scrypt values
+    memcpy(&scrypt_n, data + offset, 4);
+    offset += 4;
+    memcpy(&scrypt_r, data + offset, 4);
+    offset += 4;
+    memcpy(&scrypt_p, data + offset, 4);
+    offset += 4;
+
+    //salt & nonce
+    memcpy(salt, data + offset, 24);
+    offset += 24;
+    memcpy(nonce, data + offset, 24);
+    offset += 24;
+
+    //block two
+    memcpy(&block_two_size, data + offset, 8);
+    offset += 8;
+    memcpy(block_two_encrypted, data + offset, block_two_size);
+
+    //a nice gesture
+    memset(block_two_plaintext, 0, block_two_size);
+    memset(encrypted_key, 0, 32);
+
+    return 0;
 }
 
-void _data_init(tox_data *data) {
-    data->file_path = NULL;
-    data->locked = 1;
+int tox_load_encrypted(Tox *tox, uint8_t *data, uint32_t length, uint8_t *key, uint16_t key_length) {
+    uint32_t scrypt_n,
+             scrypt_r,
+             scrypt_p;
 
-    data->scrypt_n = 12;
-    data->scrypt_r = 8;
-    data->scrypt_p = 1;
+    uint8_t  encrypted_key[crypto_secretbox_KEYBYTES],
+             nonce[crypto_secretbox_NONCEBYTES],
+             salt[24];
 
-    data->name = NULL;
-    data->_data = NULL;
-}
+    uint64_t block_two_length;
 
-tox_data* data_init_new(char *path, uint8_t *username, uint8_t *password) {
-    tox_data *data = (tox_data*)malloc(sizeof(tox_data));
-    _data_init(data);
-
-    size_t str_size = strlen(path) + 1;
-    data->file_path = (char*)malloc(str_size);
-    strncpy(data->file_path, path, str_size);
-
-    str_size = strlen((char*)username) + 1;
-    data->name = (uint8_t*)malloc(str_size);
-    strncpy((char*)data->name, (char*)username, str_size);
-
-    _gen_key_new(data, password);
-    memset(password, 0, strlen((char*)password) + 1);
-    data->locked = 0;
-
-    return data;
-}
-
-tox_data* data_init_load(char *path) {
-    tox_data *data = (tox_data*)malloc(sizeof(tox_data));
-    _data_init(data);
-
-    size_t str_size = strlen(path) + 1;
-    data->file_path = (char*)malloc(str_size);
-    strncpy(data->file_path, path, str_size);
-
-    FILE *file = fopen(path, "r");
-    if(file == NULL)
-        return NULL;
+    int offset = 0;
 
     //check magic
     char magic[4];
-    fread(magic, 1, 4, file);
+    memcpy(data, magic, 4);
     if(memcmp(magic,&"libe",4) != 0)
         return NULL;
-
-    //read time last saved
-    fread(&data->time_saved, 8, 1, file);
-
-    //read name
-    uint16_t name_length;
-    fread(&name_length, 2, 1, file);
-    data->name = (uint8_t*)malloc(name_length);
-    fread(data->name, 1, name_length, file);
-    //check for nul-terminated name string
-    if(data->name[name_length - 1] != '\0') {
-        data->name = realloc(data->name, name_length + 1);
-        data->name[name_length] = '\0';
-    }
+    offset += 4;
 
     //scrypt vars
-    fread(&data->scrypt_n, 4, 1, file);
-    fread(&data->scrypt_r, 4, 1, file);
-    fread(&data->scrypt_p, 4, 1, file);
+    memcpy(data + offset, &scrypt_n, 4);
+    offset += 4;
+    memcpy(data + offset, &scrypt_r, 4);
+    offset += 4;
+    memcpy(data + offset, &scrypt_p, 4);
+    offset += 4;
 
     //salt & nonce
-    fread(data->salt, 1, 24, file);
-    fread(data->nonce, 1, 24, file);
+    memcpy(data + offset, salt, 24);
+    offset += 24;
+    memcpy(data + offset, nonce, 24);
+    offset += 24;
 
     //block two
-    fread(&data->_block_two_length, 8, 1, file);
-    data->_block_two_offset = ftell(file);
+    memcpy(data + offset, &block_two_length, 8);
+    offset += 8;
 
-    fclose(file);
-    return data;
-}
+    uint8_t block_two_encrypted[block_two_length],
+            block_two_plaintext[block_two_length];
 
-void data_close(tox_data *data) {
-    free(data->file_path);
-    free(data->name);
-    free(data->_data);
-    free(data);
-}
-
-int data_unlock(tox_data *data, uint8_t *password) {
-    if(!data->locked)
-        return 0;
-
-    //load encrypted block
-    FILE *file = fopen(data->file_path, "r");
-
-    uint8_t block_two_encrypted[data->_block_two_length],
-            block_two_plaintext[data->_block_two_length];
-
-    fseek(file, data->_block_two_offset, 0);
-    fread(block_two_encrypted, 1, data->_block_two_length, file);
-
-    fclose(file);
+    memcpy(data + offset, block_two_encrypted, block_two_length);
 
     //derive key from file
-    _gen_key(data, password, data->encrypted_key);
+    scrypt(key, key_length, salt, 24, scrypt_n, scrypt_r, scrypt_p, encrypted_key, 32);
 
     //decrypt block
-    if(crypto_secretbox_open(block_two_plaintext, block_two_encrypted, data->_block_two_length, data->nonce, data->encrypted_key) != 0)
+    if(crypto_secretbox_open(block_two_plaintext, block_two_encrypted, block_two_length, nonce, encrypted_key) != 0)
         return -1;
 
     //check magic
@@ -125,145 +128,11 @@ int data_unlock(tox_data *data, uint8_t *password) {
     if(memcmp(magic, &"rtas",4) != 0)
         return -2;
 
-    //load for future use
-    if(data->_data != NULL)
-        free(data->_data);
-    data->_data_length = data->_block_two_length - 36;
-    data->_data = (uint8_t*)malloc(data->_data_length);
-    memcpy(data->_data, block_two_plaintext + 36, data->_data_length);
+    tox_load(tox, block_two_plaintext + 36, block_two_length - 36);
 
-    /* Generate a new key for future saving.
-     * This somewhat more secure than keeping the user password around in plaintext in
-     * the ram (keeping in mind, of course, that it's already game-over if the client is
-     * compromised). Still though - it's the thought that counts.
-     */
-    _gen_key_new(data, password);
+    //a nice gesture
+    memset(block_two_plaintext, 0, block_two_length);
+    memset(encrypted_key, 0, 32);
 
-    memset(block_two_plaintext, 0, data->_block_two_length);
-
-    data->locked = 0;
-
-    return 0;
-}
-
-/* ------------- REQUIRES UNLOCKING ------------- */
-int data_lock(tox_data *data) {
-    if(data->locked)
-        return -1;
-
-    data_flush(data);
-
-    memset(data->_data, 0, data->_data_length);
-    memset(data->encrypted_key, 0, 32);
-
-    free(data->_data);
-    data->_data = NULL;
-
-    data->locked = 1;
-    return 0;
-}
-
-int data_change_key(tox_data *data, uint8_t *old_password, uint8_t *new_password) {
-    if(data->locked)
-        if(data_unlock(data, old_password) != 0)
-            return -1;
-
-    uint8_t key[32];
-    _gen_key(data, old_password, key);
-
-    /* Check to see if keys match.
-     * Although there's no cryptographic need to do so, it ensures that whomever's using
-     * the profile should have access.
-     */
-    if(strcmp((const char*)key, (const char*)data->encrypted_key) != 0)
-        return -2;
-
-    _gen_key_new(data, new_password);
-    return 0;
-}
-
-int data_write_messenger(tox_data *data, uint8_t *buffer, size_t length) {
-    if(data->locked)
-        return -1;
-
-    if(data->_data != NULL)
-        free(data->_data);
-
-    data->_data_length = length;
-    data->_data = (uint8_t*)malloc(length);
-    memcpy(data->_data, buffer, length);
-    data_flush(data);
-
-    return 0;
-}
-
-size_t data_messenger_size(tox_data *data) {
-    if(data->locked)
-        return -1;
-
-    return data->_data_length;
-}
-
-int data_read_messenger(tox_data *data, uint8_t *buffer) {
-    if(data->locked)
-        return -1;
-
-    memcpy(buffer, data->_data, data->_data_length);
-    return 0;
-}
-
-int data_flush(tox_data *data) {
-    if(data->locked)
-        return -1;
-
-    /* Create block two */
-    uint64_t block_two_size = data->_data_length + 36;
-    uint8_t block_two_plaintext[block_two_size], block_two_encrypted[block_two_size];
-    uint8_t magic2[4] = {0x72, 0x74, 0x61, 0x73};
-
-    memcpy(block_two_plaintext + 32, magic2, 4);
-    memcpy(block_two_plaintext + 32 + 4, data->_data, data->_data_length);
-
-    //required zerobytes
-    memset(block_two_plaintext, 0, 32);
-
-    /* Encrypt block two */
-    if(crypto_secretbox(block_two_encrypted, block_two_plaintext, block_two_size, data->nonce, data->encrypted_key) != 0)
-        return -2;
-    memset(block_two_plaintext, 0, block_two_size);
-
-    FILE *file = fopen(data->file_path, "w+");
-
-    /* Compose entire file */
-    //determine file size & create buffer
-    uint16_t name_length = strlen((char*)data->name);
-
-    //magic
-    uint8_t magic1[4] = {0x6c, 0x69, 0x62, 0x65};
-    fwrite(magic1, 1, 4, file);
-
-    //time
-    data->time_saved = time(NULL);
-    fwrite(&data->time_saved, 8, 1, file);
-
-    //profile name
-    fwrite(&name_length, 2, 1, file);
-    fwrite(data->name, 1, name_length, file);
-
-    //scrypt values
-    fwrite(&data->scrypt_n, 4, 1, file);
-    fwrite(&data->scrypt_r, 4, 1, file);
-    fwrite(&data->scrypt_p, 4, 1, file);
-
-    //salt & nonce
-    fwrite(data->salt, 1, 24, file);
-    fwrite(data->nonce, 1, 24, file);
-
-    //block two
-    fwrite(&block_two_size, 8, 1, file);
-    fwrite(block_two_encrypted, 1, block_two_size, file);
-
-    if(fclose(file) != 0)
-        return -3;
     return 0;
 }
--- tox_data.h
+++ tox_data.h
@@ -14,16 +14,13 @@ extern "C" {
 
 #include <scrypt-jane.h>
 
-/* "Profile" Save Format
+/* Buffer Save Format
  * ==============
  *
  * bytes    name        type        purpose
  * ----------------------------------------
  * -- block one [unencrypted] --
  * 4        magic       uint8       magic, 6c:69:62:65 "libe"
- * 8        saved       uint64      unix timestamp from when the profile was last used
- * 2        namelen     uint16      length of name
- * varies   name        uint8       name of profile, UTF-8 preferably
  * 12       scryptvars  uint32      N,r,p variables for scrypt - in this order
  * 24       salt        uint8       the salt for scrypt
  * 24       nonce       uint8       the nonce for nacl
@@ -34,114 +31,24 @@ extern "C" {
  * varies   profile     uint8       the messenger data - this goes to tox_load()
  */
 
-typedef struct tox_data {
-    //the file path for the given profile
-    char *file_path;
-    //indicates whether the profile is locked
-    int locked;
+/* ------------- SIMPLE-USE FUNCTIONS ------------- */
+/* return the size of data to pass to messenger_save_encrypted(...)
+ */
+uint32_t tox_size_encrypted(Tox *tox);
 
-    uint8_t encrypted_key[crypto_secretbox_KEYBYTES], //Sodium encrypt key, 32 bytes.
-            nonce[crypto_secretbox_NONCEBYTES], //Sodium nonce, 24 bytes.
-            salt[24]; //Scrypt salt, 24 bytes.
-
-    /* The values used (N=12,r=8,p=1) were sourced from here: https://www.tarsnap.com/scrypt/scrypt-slides.pdf
-     * Percival recommends larger (N=14) values for interactive logins - I've lessened them slightly to make
-     * decryption acceptable on slower machines. One can always specify harder values.
-     */
-    uint32_t scrypt_n,
-             scrypt_r,
-             scrypt_p;
-
-    //The profile's name and last save time.
-    uint8_t *name;
-    uint64_t time_saved;
-
-    //The encrypted block's file offset and length.
-    size_t _block_two_offset;
-    uint64_t _block_two_length;
-
-    //The unencrypted messenger data.
-    uint8_t *_data;
-    size_t _data_length;
-} tox_data;
-
-/* NOTE - all string function parameters must be nul-teminated (path, password, et cetera) */
-
-/* Creates and returns a new tox_data with the given path, internal name, and password
- * The returned tox_data is both unlocked and not yet saved to disk.
- * The caller is responsible for calling data_close() on the given tox_data when done.
+/* Save the messenger, encrypting the data with key of length key_length
  *
- * returns tox_data on success, NULL otherwise
+ * return 0 on success.
+ * return -1 on failure.
  */
-tox_data* data_init_new(char *path, uint8_t *data_name, uint8_t *password);
+int tox_save_encrypted(Tox *tox, uint8_t *data, uint8_t *key, uint16_t key_length);
 
-/* Loads a tox_data from the given path
- * The returned tox_data is locked until data_unlock() is called.
- * The caller is responsible for calling data_close() on the given tox_data when done.
+/* Load the messenger from data of size length encrypted with key of key_length.
  *
- * returns tox_data on success, NULL otherwise
+ * return 0 on success.
+ * return -1 on failure.
  */
-tox_data* data_init_load(char *path);
-
-/* Safely frees a given tox_data */
-void data_close(tox_data *data);
-
-/* Unlocks the given tox_data
- * 
- * returns  0 if success
- *          -1 if the password is wrong
- *          -2 if the file is malformed
- */
-int data_unlock(tox_data *data, uint8_t *password);
-
-/* ------------- REQUIRES UNLOCKING ------------- */
-
-/* Locks the given tox_data
- * 
- * returns  0 if success
- *          -1 if the file is locked
- */
-int data_lock(tox_data *data);
-
-/* Changes the password for the given tox_data
- * 
- * returns  0 if success
- *          -1 if the file is locked
- *          -2 if the old password is wrong
- */
-int data_change_key(tox_data *data, uint8_t *old_password, uint8_t *new_password);
-
-/* Writes the given messenger to the given tox_data
- * This function also calls data_flush().
- * 
- * returns  0 if success
- *          -1 if the file is locked
- */
-int data_write_messenger(tox_data *data, uint8_t *buffer, size_t length);
-
-/* Returns the size of the loaded messenger
- * 
- * returns  the size of the tox messenger if success
- *          -1 if the file is locked
- */
-size_t data_messenger_size(tox_data *data);
-
-/* Copies the messenger from the given tox_data to the given buffer
- * The buffer must be preallocated to data_messenger_size() bytes.
- * 
- * returns  0 if success
- *          -1 if the file is locked
- */
-int data_read_messenger(tox_data *data, uint8_t *buffer);
-
-/* Writes the given tox_data to the disk
- * 
- * returns  0 if success
- *          -1 if the file is locked
- *          -2 if encryption error
- *          -3 if FILE error
- */
-int data_flush(tox_data *data);
+int tox_load_encrypted(Tox *tox, uint8_t *data, uint32_t length, uint8_t *key, uint16_t key_length);
 
 #ifdef __cplusplus
 }
